bb0f1eb4c4b2804e1072ed127f34e0825df107f1
Mohammed Ali Mohammed Bajhaw <163445161+axahamimi@users.noreply.github.com>
2024-05-28 23:29:35 +0300

    first

--- SchoolSystem.DAL/Data/AppDbContext.cs
+++ SchoolSystem.DAL/Data/AppDbContext.cs
@@ -96,9 +96,9 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<VteacherEvalution> VteacherEvalutions { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source = LAPTOP-2AR5OF7M ; Initial Catalog = SchoolSystem; Integrated Security =SSPI ;TrustServerCertificate = True");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Data Source = LAPTOP-2AR5OF7M ; Initial Catalog = SchoolSystem; Integrated Security =SSPI ;TrustServerCertificate = True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
